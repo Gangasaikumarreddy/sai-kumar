f98e5d25450d7e257fc04a3031e89e379edcf7b9
sai kumar <112061391+Gangasaikumarreddy@users.noreply.github.com>
2022-08-25 22:16:58 +0530

    Add files via upload

---
+++ sum of two num in prototype.cpp
@@ -0,0 +1,15 @@
+//sum of two numbers 
+#include<iostream>
+using namespace std;
+void add() //function prototype
+{ 
+int a,b,c;
+cout<<"enter two numbers=";
+cin>>a>>b;
+c=a+b;
+cout<<c;
+} 
+main()
+{ 
+add(); //function calling
+}
