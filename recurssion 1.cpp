ee5545a679a86fb0e32dca52cd09d462f6d92e51
sai kumar <112061391+Gangasaikumarreddy@users.noreply.github.com>
2022-08-25 22:11:13 +0530

    Add files via upload

---
+++ recurssion 1.cpp
@@ -0,0 +1,21 @@
+//recurssion 
+//when fn calls itself again and again is known as recurssion 
+//factorial of number
+#include<iostream>
+using namespace std;
+int fun(int n)
+{ 
+if (n==1)
+return 1;
+else
+return n*fun(n-1);
+}
+main()
+{ 
+int n,f;
+cout<<"enter number=";
+cin>>n;
+f=fun(n);
+cout<<"factorial is= "<<f;
+}
+
