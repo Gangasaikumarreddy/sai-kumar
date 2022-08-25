ebd497171e8f1b1945f1cff1d459a488d579cd47
sai kumar <112061391+Gangasaikumarreddy@users.noreply.github.com>
2022-08-25 22:14:59 +0530

    Add files via upload

---
+++ student.cpp
@@ -0,0 +1,18 @@
+//student 
+#include<iostream>
+using namespace std;
+main()
+{ 
+int rollno;
+char name;
+void getdata();
+{ 
+cout<<"enter the rollno"<<endl;
+cin>>rollno;
+cout<<"enter the name"<<endl;
+cin>>name;
+}
+void showdata();
+cout<<"enter the rollno"<<endl;
+cout<<"enter the name"<<endl;
+}
